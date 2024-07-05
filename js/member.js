20ca57b5ab77f71bac7df4874464ab431ddf647f
Zichen "Charles" Zhang <59857814+CharlesZzzzz@users.noreply.github.com>
2024-07-06 01:37:55 +0800

    111

--- js/member.js
+++ js/member.js
@@ -1,402 +1,402 @@
 var member = [
   {
-    "phone": "",
-    "name": "1"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "2"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "3"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "4"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "5"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "6"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "7"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "8"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "9"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "10"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "11"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "12"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "13"
+    "phone": "a",
+    "name": "神秘大奖"
   },
   {
-    "phone": "",
-    "name": "14"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "15"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "16"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "17"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "18"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "19"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "20"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "21"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "22"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "23"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "24"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "25"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "26"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "27"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "28"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "29"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "30"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "31"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "32"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "33"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "34"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "35"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "36"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "37"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "38"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "39"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "40"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "41"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "42"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "43"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "44"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "45"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "46"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "47"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "48"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "49"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "50"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "51"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "52"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "53"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "54"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "55"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "56"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "57"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "58"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "59"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "60"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "61"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "62"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "63"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "64"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "65"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "66"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "67"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "68"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "69"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "70"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "71"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "72"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "73"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "74"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "75"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "76"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "77"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "78"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "79"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "80"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "81"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "82"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "83"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "84"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "85"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "86"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "87"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "88"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "89"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "90"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "91"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "92"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "93"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "94"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "95"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "96"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "97"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "98"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "99"
+    "phone": "a",
+    "name": "再来一次"
   },
   {
-    "phone": "",
-    "name": "100"
+    "phone": "a",
+    "name": "再来一次"
   }
 ]
